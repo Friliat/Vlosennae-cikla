7f9e2d6b879e764325927a9948461c8c74e26295
Friliat <rz389905@gmail.com>
2023-12-22 19:22:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double P = 1;
+            int d;
+            int f;
+            for (d= 1;d<=4;d++)
+            {
+                double S = 0;
+                for (f = 1; f <= 3; f++)
+                {
+                    S= Log(d + f / 2);
+                }
+                P = P * S;
+            }
+            Console.WriteLine($"P={P}");
+        }
+    }
+}
